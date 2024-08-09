31630c8f9382ef9486a7df1f25c90b47a562b3af
herold1989 <jens_herold@gmx.net>
2024-08-09 16:59:09 +0200

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="color-scheme" content="light only" />
       <body className={inter.className}>{children}</body>
     </html>
   );
